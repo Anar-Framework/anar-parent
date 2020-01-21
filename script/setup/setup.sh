4143c985a7d22ed416360d7f800667788e5cc6df
mohbadar <mohammadbadarhashimi@gmail.com>
2020-01-21 08:51:56 +0430

    setup shell script

--- script/setup/setup.sh
+++ script/setup/setup.sh
@@ -2,3 +2,72 @@
 
 cd ../..
 
+git clone https://github.com/Anar-Framework/anar-lib-mail.git
+
+git clone https://github.com/Anar-Framework/anar-lib-zip.git
+
+
+git clone https://github.com/Anar-Framework/anar-lib-xml.git
+
+
+git clone https://github.com/Anar-Framework/anar-lib-uuid.git
+
+git clone https://github.com/Anar-Framework/anar-lib-pdf.git
+
+
+git clone https://github.com/Anar-Framework/anar-lib-math.git
+
+git clone https://github.com/Anar-Framework/anar-lib-logger.git
+
+git clone https://github.com/Anar-Framework/anar-lib-json.git
+
+
+git clone https://github.com/Anar-Framework/anar-lib-hmac.git
+
+git clone https://github.com/Anar-Framework/anar-lib-hash.git
+
+git clone https://github.com/Anar-Framework/anar-lib-excel.git
+
+
+git clone https://github.com/Anar-Framework/anar-lib-emptycheck.git
+
+git clone https://github.com/Anar-Framework/anar-lib-date.git
+
+git clone https://github.com/Anar-Framework/anar-lib-calender.git
+
+git clone https://github.com/Anar-Framework/anar-lib-checksum.git
+
+git clone https://github.com/Anar-Framework/anar-lib-crypto.git
+
+
+git clone https://github.com/Anar-Framework/anar-lib-crypto-jce.git
+
+git clone https://github.com/Anar-Framework/anar-lib-csv.git
+
+git clone https://github.com/Anar-Framework/anar-lib-servicetemplate.git
+
+git clone https://github.com/Anar-Framework/anar-lib-webapptemplate.git
+
+git clone https://github.com/Anar-Framework/anar-lib-activemq.git
+
+git clone https://github.com/Anar-Framework/anar-lib-kafka.git
+
+git clone https://github.com/Anar-Framework/anar-lib-workflow.git
+
+git clone https://github.com/Anar-Framework/anar-lib-core.git
+
+git clone https://github.com/Anar-Framework/anar-lib-cors.git
+
+git clone https://github.com/Anar-Framework/anar-lib-connect.git
+
+git clone https://github.com/Anar-Framework/anar-keycloak-theme.git
+
+git clone https://github.com/Anar-Framework/anar-keycloak.git
+
+git clone https://github.com/Anar-Framework/anar-lib-str.git
+
+git clone https://github.com/Anar-Framework/anar-api.git
+
+git clone https://github.com/Anar-Framework/anar-docs.git
+
+git clone https://github.com/Anar-Framework/Anar-Identity.git
