afdd279eb9357ab7abfef295bbd19347daa8b167
Mirwais Akrami <m7akrami770@gmail.com>
2020-01-26 14:36:39 +0430

    Update setup.sh

--- script/setup/setup.sh
+++ script/setup/setup.sh
@@ -1,90 +1,59 @@
+# -----------------------------------------------------------------------------
+# @author Mirwais Akrami
+# @gmail m7akrami770@gmail.com
+# Script to clone all Anar Framework repositories into a parent directory
+# for an easier developer experience.
+# -----------------------------------------------------------------------------
+#!/bin/bash
 
 
+declare -A REPOLIST
+regex="(git\:).*(\.git)"
+repoNameReg="[^\/]+$"
+
+# Get the list of repositories in Anar Framework
+REPOS=$(curl -X GET https://api.github.com/orgs/Anar-Framework/repos)
+
+# go to the parent directory first
 cd ../..
 
-git clone https://github.com/Anar-Framework/anar-lib-mail.git
+extract() {
+    if [ $1 == '"git_url":' ]; then
+        if [[ $2 =~ $regex ]]; then
+            GIT_URL="${BASH_REMATCH[0]}"
 
-git clone https://github.com/Anar-Framework/anar-lib-zip.git
+            # Get repo name
+            if [[ $GIT_URL =~ $repoNameReg ]]; then
+                # remove the .git part
+                if [[ "${BASH_REMATCH[0]}" =~ ^([^\.])+ ]]; then
+                    REPOLIST["${BASH_REMATCH[0]}"]="$GIT_URL"
+                fi
+            else
+                echo "Not found"
+            fi
+        fi
+    fi
+}
 
-git clone https://github.com/Anar-Framework/anar-lib-audit.git
+while IFS= read -r line
+do
+    # Pass one line of response at a time
+    extract $line
+done < <(printf '%s\n' "$REPOS")
 
+# reset IFS to default back
+IFS=' '
 
-git clone https://github.com/Anar-Framework/anar-lib-api.git
-
-git clone https://github.com/Anar-Framework/anar-lib-xml.git
-
-git clone https://github.com/Anar-Framework/anar-lib-file.git
-
-git clone https://github.com/Anar-Framework/anar-lib-uuid.git
-
-git clone https://github.com/Anar-Framework/anar-lib-pdf.git
-
-
-git clone https://github.com/Anar-Framework/anar-lib-math.git
-
-git clone https://github.com/Anar-Framework/anar-lib-logger.git
-
-git clone https://github.com/Anar-Framework/anar-lib-json.git
-
-
-git clone https://github.com/Anar-Framework/anar-lib-hmac.git
-
-git clone https://github.com/Anar-Framework/anar-lib-hash.git
-
-git clone https://github.com/Anar-Framework/anar-lib-excel.git
-
-
-git clone https://github.com/Anar-Framework/anar-lib-emptycheck.git
-
-git clone https://github.com/Anar-Framework/anar-lib-date.git
-
-git clone https://github.com/Anar-Framework/anar-lib-calendar.git
-
-git clone https://github.com/Anar-Framework/anar-lib-checksum.git
-
-git clone https://github.com/Anar-Framework/anar-lib-crypto.git
-
-
-git clone https://github.com/Anar-Framework/anar-lib-crypto-jce.git
-
-git clone https://github.com/Anar-Framework/anar-lib-csv.git
-
-
-
-git clone https://github.com/Anar-Framework/anar-lib-activemq.git
-
-git clone https://github.com/Anar-Framework/anar-lib-kafka.git
-
-git clone https://github.com/Anar-Framework/anar-lib-workflow.git
-
-git clone https://github.com/Anar-Framework/anar-lib-core.git
-
-git clone https://github.com/Anar-Framework/anar-lib-cors.git
-
-
-git clone https://github.com/Anar-Framework/anar-keycloak-theme.git
-
-git clone https://github.com/Anar-Framework/anar-keycloak.git
-
-git clone https://github.com/Anar-Framework/anar-lib-str.git
-
-git clone https://github.com/Anar-Framework/anar-lib-api.git
-
-git clone https://github.com/Anar-Framework/anar-lib-metrics.git
-
-git clone https://github.com/Anar-Framework/anar-docs.git
-
-git clone https://github.com/Anar-Framework/Anar-Identity.git
-
-git clone https://github.com/Anar-Framework/anar-datacollection-kit.git
-
-git clone https://github.com/Anar-Framework/anar-servicetemplate.git
-
-git clone https://github.com/Anar-Framework/anar-webapptemplate.git
-
-git clone https://github.com/Anar-Framework/anar-connect.git
-
-
-git clone https://github.com/Anar-Framework/anar-identity-custom.git
-
-git clone https://github.com/Anar-Framework/anar-servicetemaplate-customidentity.git
+if [[ "${#REPOLIST[@]}" > 0 ]]; then
+    # Clone the repos if not already cloned
+    for val in "${!REPOLIST[@]}"; do
+        if [[ -d $val ]]; then
+            echo -e "\e[32mRepo ${val} already exist\e[39m"
+        else 
+            echo -e "\e[33mRepository ${val} does not exist\e[39m"
+            git clone ${REPOLIST[$val]}
+        fi
+    done
+    else
+        echo "No repositories available in Anar"
+fi
