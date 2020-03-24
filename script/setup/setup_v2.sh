67ef1909c42f0c894c9b2e17e903946f08d9e1e9
Hameedullah Pardess <hameedullah.pardess@yahoo.com>
2020-03-24 16:26:47 +0430

    Update setup_v2.sh

--- script/setup/setup_v2.sh
+++ script/setup/setup_v2.sh
@@ -1,5 +1,53 @@
 #!/bin/bash
 mkdir -p ./../anar-framework
 
-git clone https://<username>:<password>@github.com/Anar-Framework/anar-lib-kafka.git ./../anar-framework/anar-lib-kafka
-git clone https://<username>:<password>@github.com/Anar-Framework/anar-lib-workflow.git ./../anar-framework/anar-lib-workflow
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-accounting ./../anar-framework/anar-accounting
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-connect ./../anar-framework/anar-connect
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-keycloack ./../anar-framework/anar-keycloack
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-lib-activemq ./../anar-framework/anar-lib-activemq
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-lib-api ./../anar-framework/anar-lib-api
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-lib-audit ./../anar-framework/anar-lib-audit
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-lib-calendar ./../anar-framework/anar-lib-calendar
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-lib-checksum ./../anar-framework/anar-lib-checksum
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-lib-core ./../anar-framework/anar-lib-core
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-lib-cors ./../anar-framework/anar-lib-cors
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-lib-crypto ./../anar-framework/anar-lib-crypto
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-lib-crypto-jce ./../anar-framework/anar-lib-crypto-jce
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-lib-csv ./../anar-framework/anar-lib-csv
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-lib-date ./../anar-framework/anar-lib-date
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-lib-emptycheck ./../anar-framework/anar-lib-emptycheck
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-lib-excel ./../anar-framework/anar-lib-excel
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-lib-file ./../anar-framework/anar-lib-file
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-lib-hash ./../anar-framework/anar-lib-hash
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-lib-hmac ./../anar-framework/anar-lib-hmac
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-lib-json ./../anar-framework/anar-lib-json
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-lib-kafka ./../anar-framework/anar-lib-kafka
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-lib-logger ./../anar-framework/anar-lib-logger
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-lib-mail ./../anar-framework/anar-lib-mail
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-lib-math ./../anar-framework/anar-lib-math
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-lib-pdf ./../anar-framework/anar-lib-pdf
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-lib-str ./../anar-framework/anar-lib-str
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-lib-uuid ./../anar-framework/anar-lib-uuid
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-lib-workflow ./../anar-framework/anar-lib-workflow
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-lib-xml ./../anar-framework/anar-lib-xml
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-lib-zip ./../anar-framework/anar-lib-zip
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-lib-metrics ./../anar-framework/anar-lib-metrics
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-lib-velocitytemplate ./../anar-framework/anar-lib-velocitytemplate
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-lib-reportutil ./../anar-framework/anar-lib-reportutil
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-lib-jobs ./../anar-framework/anar-lib-jobs
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-lib-html ./../anar-framework/anar-lib-html
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-docs ./../anar-framework/anar-docs
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-keycloak-theme ./../anar-framework/anar-keycloak-theme
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-servicetemplate ./../anar-framework/anar-servicetemplate
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-webapptemplate ./../anar-framework/anar-webapptemplate
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-keycloak ./../anar-framework/anar-keycloak
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-datacollection-kit ./../anar-framework/anar-datacollection-kit
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-identity-custom ./../anar-framework/anar-identity-custom
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-servicetemaplate-customidentity ./../anar-framework/anar-servicetemaplate-customidentity
+
+
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-assetmanagement ./../anar-framework/anar-assetmanagement
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-spm ./../anar-framework/anar-spm
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-cache ./../anar-framework/anar-cache
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-hooks ./../anar-framework/anar-hooks
+git clone https://<username>:<password>@github.com/Anar-Framework/anar-adhocquery ./../anar-framework/anar-adhocquery
